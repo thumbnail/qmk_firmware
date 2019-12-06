0749177e83282c2c88a7a83fd5d27cb7f59aadab
Jeroen de Jong <thumbnail95@gmail.com>
2019-12-06 09:15:45 +0100

    fix backslash, dups and alt

--- keyboards/redox_w/keymaps/thumbnail/keymap.c
+++ keyboards/redox_w/keymaps/thumbnail/keymap.c
@@ -30,12 +30,11 @@ enum custom_keycodes {
 #define KC_ADPU LT(_ADJUST, KC_PGUP)
 
 #define C_SYMB  LT(_SYMB,KC_GRV)
-#define C_ALT   LALT(KC_LSFT)
 #define C_CMD   GUI_T(KC_QUOT)
-#define C_HYPER ALL_T(KC_NO)
-#define C_MEH   MEH_T(KC_NO)
 #define C_CTRLZ CTL_T(KC_Z)
 #define C_ESC   CTL_T(KC_ESC)
+#define C_HOME  GUI_T(KC_HOME)
+#define C_EMOIJ MOD_LCTL | MOD_LGUI | KC_SPC
 
 #define C_MEDIA LT(_MEDIA, KC_SCLN)
 
@@ -45,13 +44,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_EQL  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                                            KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_NAMI ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_DEL  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,KC_LBRC ,                          KC_RBRC ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_EQL  ,
+     KC_DEL  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,KC_LBRC ,                          KC_RBRC ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_BSLS ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_BSPC ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,C_HYPER ,                          C_MEH   ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,C_MEDIA ,C_CMD   ,
+     KC_BSPC ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,MOD_HYPR,                          MOD_MEH ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,C_MEDIA ,C_CMD   ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_ADPU ,KC_HOME ,        KC_LALT ,C_ESC   ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSFT ,
+     KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_ADPU ,C_HOME  ,        KC_LALT ,C_ESC   ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSFT ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     C_SYMB  ,KC_LCTRL,C_ALT   ,KC_LEFT ,     KC_RIGHT,    KC_SPC  ,KC_ADEN ,        KC_TAB  ,KC_ENT  ,    KC_UP   ,     KC_DOWN ,KC_DOWN ,KC_UP   ,KC_RGHT
+     C_SYMB  ,KC_LCTRL,KC_LALT ,KC_LEFT ,     KC_RIGHT,    KC_SPC  ,KC_ADEN ,        KC_TAB  ,KC_ENT  ,    KC_UP   ,     KC_DOWN ,KC_RALT ,KC_RCTRL,C_EMOIJ
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
